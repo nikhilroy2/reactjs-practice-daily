482d248e8e25ec9629a02d7c99a9f010ee63ae38
nikhilroy2 <nikhilchandraroypoet@gmail.com>
2020-05-08 22:47:03 +0600

    Updates

---
+++ precache-manifest.862127c71d6550a8cbea96464faca466.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7582ad2b53c341e7106d90ddcecc130d",
+    "url": "/reactjs-daily-practice/index.html"
+  },
+  {
+    "revision": "1ae31828c9daa86305f0",
+    "url": "/reactjs-daily-practice/static/css/main.b345cbab.chunk.css"
+  },
+  {
+    "revision": "a3503b374efb558b5a24",
+    "url": "/reactjs-daily-practice/static/js/2.61e3bd08.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/reactjs-daily-practice/static/js/2.61e3bd08.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1ae31828c9daa86305f0",
+    "url": "/reactjs-daily-practice/static/js/main.25125a02.chunk.js"
+  },
+  {
+    "revision": "e7d996e6accda4d43a80",
+    "url": "/reactjs-daily-practice/static/js/runtime-main.25da185f.js"
+  },
+  {
+    "revision": "1a2a4a6b42a2c627128e1f09c3657e63",
+    "url": "/reactjs-daily-practice/static/media/img1.1a2a4a6b.png"
+  },
+  {
+    "revision": "579de8a3eed9d594b741b8b5e9380053",
+    "url": "/reactjs-daily-practice/static/media/img2.579de8a3.png"
+  },
+  {
+    "revision": "2093a58e9a5cb2f2b328f219e4255800",
+    "url": "/reactjs-daily-practice/static/media/img3.2093a58e.png"
+  },
+  {
+    "revision": "e421e0ff93b9931944ecbc70238d2020",
+    "url": "/reactjs-daily-practice/static/media/img4.e421e0ff.png"
+  },
+  {
+    "revision": "112ef05fdec8110491c3ecc13b9e8baf",
+    "url": "/reactjs-daily-practice/static/media/img5.112ef05f.png"
+  },
+  {
+    "revision": "3711527b7ccdd00908bb9d612afdbffe",
+    "url": "/reactjs-daily-practice/static/media/img6.3711527b.png"
+  },
+  {
+    "revision": "16d1144c30dc3305230cbe5a8c77cc84",
+    "url": "/reactjs-daily-practice/static/media/minimo.16d1144c.jpg"
+  }
+]);
